2d3d78bd3ccfa6196d662e89983041899427e730
7essen <hussona4635@gmail.com>
2024-09-09 01:35:49 +0300

    ad

--- config/database.js
+++ config/database.js
@@ -1,25 +1,14 @@
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'mongo'); // استخدام 'mongo' بدلاً من 'sqlite' أو غيرها
-
-  const connections = {
-    mongo: {
-      connection: {
-        uri: env('DATABASE_URL', 'mongodb://localhost:27017/strapi'), // استخدام URI قاعدة البيانات الخاصة بك
-        options: {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          // أضف خيارات أخرى هنا إذا لزم الأمر
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        uri: env('DATABASE_URI'),
+      },
+      options: {
+        ssl: true,
       },
     },
-    // أضف أي نوع من قواعد البيانات الأخرى هنا إذا لزم الأمر
-  };
-
-  return {
-    connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-    },
-  };
-};
+  },
+});
